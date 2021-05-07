d771f3fe2c5338efa24783dc41ed32c14b6277ca
thomasfriedrichs <friedrichs.tommy@gmail.com>
2021-05-07 15:05:04 -0700

    address inconsistencies in price

--- Bakery.Solution/Bakery/Models/Bread.cs
+++ Bakery.Solution/Bakery/Models/Bread.cs
@@ -14,12 +14,15 @@ namespace Bakery
     public static int PriceCalc(int BreadAmount)
     {
       int price = 0;
-      if (BreadAmount % 3 == 0) 
+      if (BreadAmount < 2) {
+        price = BreadAmount * 5;
+      }
+      else if (BreadAmount > 2 && BreadAmount % 3 == 0) 
       {
-        price = (int)Math.Round((BreadAmount * 5) * (2/3m));
+        price = (int)Math.Round((BreadAmount * 5) * (2/3m)); 
       } else 
       {
-        price = BreadAmount * 5;
+        price = BreadAmount * 5;  
       }
       return price;
     }
