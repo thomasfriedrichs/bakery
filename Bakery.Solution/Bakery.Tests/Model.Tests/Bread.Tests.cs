4390f1fdc1723c53550a429d1ad9b88b5bfbb484
thomasfriedrichs <friedrichs.tommy@gmail.com>
2021-05-07 09:19:44 -0700

    add test template and parent brackets

---
+++ Bakery.Solution/Bakery.Tests/Model.Tests/Bread.Tests.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class BreadTests
+  {
+    [TestMethod]
+    public void PriceCalc_CalculatePrice_5()
+    {
+      
+    }
+  }
+}
---
+++ Bakery.Solution/Bakery/Models/Bread.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Bakery
+{
+  public class Bread 
+  {
+    
+  }
+}
