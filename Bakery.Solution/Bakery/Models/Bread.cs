244cac8272ae83d581a938ac0841c944811f0958
thomasfriedrichs <friedrichs.tommy@gmail.com>
2021-05-07 15:29:39 -0700

    add attempt to address flawed logic

--- Bakery.Solution/Bakery/Models/Bread.cs
+++ Bakery.Solution/Bakery/Models/Bread.cs
@@ -14,16 +14,20 @@ namespace Bakery
     public static int PriceCalc(int BreadAmount)
     {
       int price = 0;
+      int freeLoaf = 0;
+      if (BreadAmount % 3 == 0)
+      {
+        freeLoaf = BreadAmount / 3;
+      }
+
+
       if (BreadAmount < 2) {
         price = BreadAmount * 5;
       }
       else if (BreadAmount > 2 && BreadAmount % 3 == 0) 
       {
         price = (int)Math.Round((BreadAmount * 5) * (2/3m)); 
-      } else 
-      {
-        price = BreadAmount * 5;  
-      }
+      } 
       return price;
     }
   }
