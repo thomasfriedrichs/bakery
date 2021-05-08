67922fed9313fbf1712226e4ebbb522d389dc301
thomasfriedrichs <friedrichs.tommy@gmail.com>
2021-05-07 20:49:38 -0700

    add passing object constructor

---
+++ Bakery.Solution/Bakery/Models/Pastry.cs
@@ -0,0 +1,12 @@
+namespace Bakery 
+{
+  public class Pastry
+  {
+    public int PastryAmount {get;set;}
+
+    public Pastry(int pastryAmount)
+    {
+      PastryAmount = pastryAmount;
+    }
+  }
+}
