bd54d081dc13f4a758d58a738bfeae2c874d806a
thomasfriedrichs <friedrichs.tommy@gmail.com>
2021-05-09 14:00:40 -0700

    add console app

--- Bakery.Solution/Bakery/Models/Pastry.cs
+++ Bakery.Solution/Bakery/Models/Pastry.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace Bakery 
+namespace Bakery
 {
   public class Pastry
   {
---
+++ Bakery.Solution/Bakery/Program.cs
@@ -0,0 +1,47 @@
+using System;
+
+namespace Bakery
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Hello! Welcome to Pierres, what would you like to order? loaves are 1 for 5 dollars and buy 2 get 1 free, and pastries and 2 dollars each with a 3 for 5 deal. Enter 'b' for bread and/or 'p' for pastry!");
+      String Pob = Console.ReadLine().ToLower();
+      if (Pob == "bp" || Pob == "pb")
+      {
+        Console.WriteLine("How many loaves of bread would you like? Enter a number");
+        int Bread = int.Parse(Console.ReadLine());
+        Console.WriteLine("How many pastries would you like?");
+        int Pastry = int.Parse(Console.ReadLine());
+        Console.WriteLine("Thank you, your order total is below!");
+        int PastryTotal = Pastry.PriceCalc(Pastry);
+        int BreadTotal = Bread.PriceCalc(Bread);
+        Console.WriteLine(BreadTotal + PastryTotal);
+      }
+      else if (Pob == "p")
+      {
+        Console.WriteLine("How many pastries would you like?");
+        int Pastry = int.Parse(Console.ReadLine());
+        Console.WriteLine("Thank you, your order total is below!");
+        int PastryTotal = Pastry.PriceCalc(Pastry);
+        Console.WriteLine(PastryTotal);
+      }
+      else if (Pob == "b")
+      {
+        Console.WriteLine("How many loaves of bread would you like? Enter a number");
+        int Bread = int.Parse(Console.ReadLine());
+        Console.WriteLine("Thank you, your order total is below!");
+        int BreadTotal = Bread.PriceCalc(Bread);
+        Console.WriteLine(BreadTotal);
+      } 
+      else 
+      {
+        Console.WriteLine("We're sorry, something went wrong. Please try again with the response 'p' and/or 'b'. Thank you!");
+        Main();
+      }
+
+      
+    }
+  }
+}
